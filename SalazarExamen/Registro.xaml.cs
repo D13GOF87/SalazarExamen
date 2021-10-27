f4b808deaf06ded1a41899b4a89b8ff9adda8e73
Diego Salazar <diogo_mil10@hotmail.com>
2021-10-26 19:06:46 -0500

    Agregar archivos de proyecto.

---
+++ SalazarExamen/Registro.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SalazarExamen
+{
+	public partial class Registro : ContentPage
+	{
+		public Registro()
+		{
+			InitializeComponent();
+		}
+	}
+}
